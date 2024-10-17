7ccafcc9227ccfc606e4e1e2e93fef59577633cb
Dennis Josefsson <dennis.josefsson@gmail.com>
2024-10-17 18:45:28 +0200

    Minor design tweak, team streaks component

--- frontend/src/components/Components/SingleTeam/TeamCuriosities.tsx
+++ frontend/src/components/Components/SingleTeam/TeamCuriosities.tsx
@@ -9,32 +9,45 @@ const TeamCuriosities = () => {
   return (
     <div>
       <TeamSeasonCuriosities />
-      {streaks.streakObjectsLength > 0 ? (
-        <div className="grid grid-cols-1 gap-1 lg:grid-cols-2 lg:gap-2">
+
+      <div className="grid grid-cols-1 gap-1 lg:grid-cols-2 lg:gap-2">
+        {streaks.unbeatenStreak.length > 0 ? (
           <StreakComponent>
             <StreakComponent.Title>Obesegrade</StreakComponent.Title>
             <StreakComponent.Content
               streak={streaks.unbeatenStreak}
             ></StreakComponent.Content>
           </StreakComponent>
+        ) : null}
+
+        {streaks.winStreak.length > 0 ? (
           <StreakComponent>
             <StreakComponent.Title>Vinster i rad</StreakComponent.Title>
             <StreakComponent.Content
               streak={streaks.winStreak}
             ></StreakComponent.Content>
           </StreakComponent>
+        ) : null}
+
+        {streaks.drawStreak.length > 0 ? (
           <StreakComponent>
             <StreakComponent.Title>Oavgjorda</StreakComponent.Title>
             <StreakComponent.Content
               streak={streaks.drawStreak}
             ></StreakComponent.Content>
           </StreakComponent>
+        ) : null}
+
+        {streaks.losingStreak.length > 0 ? (
           <StreakComponent>
             <StreakComponent.Title>Förluster</StreakComponent.Title>
             <StreakComponent.Content
               streak={streaks.losingStreak}
             ></StreakComponent.Content>
           </StreakComponent>
+        ) : null}
+
+        {streaks.noWinStreak.length > 0 ? (
           <StreakComponent>
             <StreakComponent.Title>
               Matcher i rad utan vinst
@@ -43,8 +56,8 @@ const TeamCuriosities = () => {
               streak={streaks.noWinStreak}
             ></StreakComponent.Content>
           </StreakComponent>
-        </div>
-      ) : null}
+        ) : null}
+      </div>
     </div>
   )
 }
